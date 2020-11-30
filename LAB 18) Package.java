8458906b95bf07c3585e312774f442f4a540c55f
LIKHITHAGOGULAPATI <69568809+LIKHITHAGOGULAPATI@users.noreply.github.com>
2020-11-30 07:53:23 +0530

    Add files via upload

---
+++ LAB 18) Package.java
@@ -0,0 +1,16 @@
+//save by A.java  
+package pack;  
+public class A {  
+    public void msg() {
+        System.out.println("Hello");
+    }  
+}  
+
+//save by B.java  
+package mypack;  
+class B {  
+    public static void main(String args[]) {  
+        pack.A obj = new pack.A();  //using fully qualified name  
+        obj.msg();  
+    }  
+}
